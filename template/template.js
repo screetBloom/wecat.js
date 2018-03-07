d8301c4810de5286ba708dbecc32cae4b139da94
screetBloom <15306225869@163.com>
2018-03-08 00:21:13 +0800

    模板引擎的源代码

--- template/template.js
+++ template/template.js
@@ -6,23 +6,32 @@
  *
  */
 
-function TemplateEngine(tpl, options) {
-    var reg = /{{([\s\S]+?}*)}}/g,
-        reExp = /(^( )?(if|for|else|switch|case|break|{|}))(.*)?/g,
-        code = 'var r=[];\n',
-        cursor = 0,
-        match;
+(function () {
 
-    var add = function (line, js) {
-        js ? (code += line.match(reExp) ? line + '\n' : 'r.push(' + line + ');\n') :
-            (code += line != '' ? 'r.push("' + line.replace(/"/g, '\\"') + '");\n' : '');
-        return add;
+    function TemplateEngine(tpl, options) {
+        var reg = /{{([\s\S]+?}*)}}/g,
+            reExp = /(^( )?(if|for|else|switch|case|break|{|}))(.*)?/g,
+            code = 'var r=[];\n',
+            cursor = 0,
+            match;
+
+        var add = function (line, js) {
+            js ? (code += line.match(reExp) ? line + '\n' : 'r.push(' + line + ');\n') :
+                (code += line != '' ? 'r.push("' + line.replace(/"/g, '\\"') + '");\n' : '');
+            return add;
+        }
+        while (match = reg.exec(tpl)) {
+            add(tpl.slice(cursor, match.index))(match[1], true);
+            cursor = match.index + match[0].length
+        }
+        code += 'return r.join("");'
+        return new Function(code.replace(/[\r\t\n]/g, '')).apply(options)
     }
-    while (match = reg.exec(tpl)) {
-        add(tpl.slice(cursor, match.index))(match[1], true);
-        cursor = match.index + match[0].length
-    }
-    code += 'return r.join("");'
-    return new Function(code.replace(/[\r\t\n]/g, '')).apply(options)
-}
+
+    window._$template = new TemplateEngine();
+
+})()
+
+
+
 
