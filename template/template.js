3353117dd87a8b29ab62332e4e680e3fed862554
screetBloom <707885370@qq.com>
2018-03-08 15:02:06 +0800

    模板引擎导出

--- template/template.js
+++ template/template.js
@@ -1,37 +1,31 @@
-/**
- *
- * User: wim_chen
- * Date: 2018/3/8
- * Time: 上午12:04
- *
- */
-
 (function () {
 
-    function TemplateEngine(tpl, options) {
-        var reg = /{{([\s\S]+?}*)}}/g,
-            reExp = /(^( )?(if|for|else|switch|case|break|{|}))(.*)?/g,
-            code = 'var r=[];\n',
-            cursor = 0,
-            match;
+	function tplFactory() {
 
-        var add = function (line, js) {
-            js ? (code += line.match(reExp) ? line + '\n' : 'r.push(' + line + ');\n') :
-                (code += line != '' ? 'r.push("' + line.replace(/"/g, '\\"') + '");\n' : '');
-            return add;
-        }
-        while (match = reg.exec(tpl)) {
-            add(tpl.slice(cursor, match.index))(match[1], true);
-            cursor = match.index + match[0].length
-        }
-        code += 'return r.join("");'
-        return new Function(code.replace(/[\r\t\n]/g, '')).apply(options)
-    }
+	}
+	tplFactory.prototype = {
+		parse: function (tpl, options) {
+			var reg = /{{([\s\S]+?}*)}}/g,
+				reExp = /(^( )?(if|for|else|switch|case|break|{|}))(.*)?/g,
+				code = 'var r=[];\n',
+				cursor = 0,
+				match;
 
-    window._$template = new TemplateEngine();
-
+			var add = function (line, js) {
+				js ? (code += line.match(reExp) ? line + '\n' : 'r.push(' + line + ');\n') :
+					(code += line != '' ? 'r.push("' + line.replace(/"/g, '\\"') + '");\n' : '');
+				return add;
+			}
+			while (match = reg.exec(tpl)) {
+				add(tpl.slice(cursor, match.index))(match[1], true);
+				cursor = match.index + match[0].length
+			}
+			code += 'return r.join("");'
+			return new Function(code.replace(/[\r\t\n]/g, '')).apply(options)
+		}
+	}
+	window._$template = new tplFactory();
 })()
-
 
 
 
