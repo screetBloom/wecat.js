000a0d6c537b9e9f75698112e990e4c4705b844c
screetBloom <15306225869@163.com>
2018-05-17 20:59:27 +0800

    使用rollup作为模块捆绑器,使用krama测试

--- wecat/rollup/src/util/index.js
+++ wecat/rollup/src/util/index.js
@@ -7,6 +7,14 @@ export function isObject (obj) {
     return obj !== null && typeof obj === 'object'
 }
 
+export function def (obj, key, val, enumerable) {
+    Object.defineProperty(obj, key, {
+        value: val,
+        enumerable: !!enumerable,
+        writable: true,
+        configurable: true
+    })
+}
 
 
 
